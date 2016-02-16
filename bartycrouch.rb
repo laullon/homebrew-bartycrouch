1aa6698792a9e7b4634e584303038b7dbc8196eb
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-02-16 12:03:35 +0100

    Update to version 0.3.0

--- bartycrouch.rb
+++ bartycrouch.rb
@@ -1,9 +1,9 @@
 class Bartycrouch < Formula
   desc "Incrementally update your Storyboard localizations with ease"
   homepage "https://github.com/Flinesoft/BartyCrouch"
-  url "https://github.com/Flinesoft/BartyCrouch/archive/0.2.4.tar.gz"
-  version "0.2.4"
-  sha256 "a196536a075c75b9f22df80f99e0c4b12dc64b7978224d91d55c2211ba86355c"
+  url "https://github.com/Flinesoft/BartyCrouch/archive/0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "e17aa7d99e6f9c51cf053a7b6477042a999b38bbb184b12f03cc3cb9286761ab"
 
   depends_on :xcode
 
