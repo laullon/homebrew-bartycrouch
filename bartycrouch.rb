f5553563ea4a71c50a5ec02c77ffcfbd31ceb5dd
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-03-03 22:54:38 +0100

    Update to version 1.3.0

--- bartycrouch.rb
+++ bartycrouch.rb
@@ -1,9 +1,9 @@
 class Bartycrouch < Formula
   desc "Automatically update/translate your .strings files on build (reacting to Storyboard/XIB changes)"
   homepage "https://github.com/Flinesoft/BartyCrouch"
-  url "https://github.com/Flinesoft/BartyCrouch/archive/1.2.1.tar.gz"
-  version "1.2.1"
-  sha256 "23a61044f19a70851e211e3ca88a42443e4bbca11e6c0cd4de2f515de32dafff"
+  url "https://github.com/Flinesoft/BartyCrouch/archive/1.3.0.tar.gz"
+  version "1.3.0"
+  sha256 "19f2596d8301270a3d313bc3adc72274cae19f36b9fc1fda07868c92ec27d0ee"
 
   depends_on :xcode
 
