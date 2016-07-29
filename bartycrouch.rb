34f1bd046ea5448df4fcd7d25cfab2656a3a64c2
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-07-29 20:19:14 +0200

    Update version to 3.2.2

--- bartycrouch.rb
+++ bartycrouch.rb
@@ -1,9 +1,9 @@
 class Bartycrouch < Formula
   desc "Incrementally update & translate your Strings files."
   homepage "https://github.com/Flinesoft/BartyCrouch"
-  url "https://github.com/Flinesoft/BartyCrouch/archive/3.2.1.tar.gz"
-  version "3.2.1"
-  sha256 "d0222843bec8bc2399f74613411d515dcd71d2dad1c9cddfba5381f6b35eb18d"
+  url "https://github.com/Flinesoft/BartyCrouch/archive/3.2.2.tar.gz"
+  version "3.2.2"
+  sha256 "4535fe4fe2821b795500e7fdb29bce1e36e6711961f45bd4f23e6424c049370a"
 
   depends_on :xcode
 
